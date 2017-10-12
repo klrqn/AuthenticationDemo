68ed977d8c8e87bbad751c3e153a2764f2ca677e
Scott Bruce Burton <scottbruceburton@gmail.com>
2017-10-12 17:49:11 +0000

    add passport

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-var express = require("express");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    User                    = require("./models/user");
+
+//MongoDB Location
+mongoose.connect("mongodb://localhost/auth_demo", {useMongoClient: true});
+mongoose.Promise = global.Promise; 
 
 var app = express();
+
+app.use(require("express-session")({
+    secret: "this string is used to encode and decode",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// take data from session - encode and decode it
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home");
+});
+
+app.get("/secret", function(req, res){
+    res.render("secret");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
